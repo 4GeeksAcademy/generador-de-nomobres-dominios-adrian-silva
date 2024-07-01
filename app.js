6c6e0ce9d9d16a27fa7a5b84bf9409eda2d57fc5
adrian-silva-uy <135650343+adrian-silva-uy@users.noreply.github.com>
2024-07-01 14:59:07 +0000

    bonnus added

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 let pronombres = ['the', 'our'];
 let adjetivos = ['great', 'big'];
 let sustantivos = ['jogi', 'racoo'];
-let extenxsiones = [ '.com', '.net', '.us', '.io'];
+let extenxsiones = ['.com', '.net', '.us', '.io'];
 
-for (let i = 0; i < pronombres.length; i++) {
-    for (let j = 0; j < adjetivos.length; j++) {
-        for (let k = 0; k < sustantivos.length; k++) {
-            for (let l = 0; l < extenxsiones.length; l++) {
-                console.log(pronombres[i] + adjetivos[j] + sustantivos[k] + extenxsiones[l]);                
+for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+            for (let l = 0; l < extension.length; l++) {
+                if (noun[k].substring(noun[k].length - 3) == extension[l].substring(extension[l].length - 3)) {
+                    console.log(pronoun[i] + adj[j] + noun[k].substring(0, noun[k].length - 3) + extension[l]);
+                } else if (noun[k].substring(noun[k].length - 2) == extension[l].substring(extension[l].length - 2)) {
+                    console.log(pronoun[i] + adj[j] + noun[k].substring(0, noun[k].length - 2) + extension[l]);
+                } else {
+                    console.log(pronoun[i] + adj[j] + noun[k] + extension[l]);
+                }
             }
         }
     }
